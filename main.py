54541ae618bc96425c7732954cd8d8aad7eba10b
unknown <baha20022612gmail.com>
2021-10-12 20:47:00 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+dateOfBirthday = 1994  # Camel case
+date_Of_birthday = 1993  # Snake case
+var_int = 9  # Variable integer (число)
+var_float = 1.2  # variable float (числа с плавающей точкой
+var_str = "bakberdi"  # variable sting (строка)
+var_boolean_true = True  # variable boolean (Истина)
+var_boolean_false = False  # variable boolean (лож)
+var_list = [1, 3, 4, "baki", [1, 4, 5, ], (1, 4, 5), {"name": "Baki"}]  # variable list (список)
+var_turple = (1, (1, 2), "Baki",)  # variable list (кортеж)
+var_dict = {"name": "baki", "date_of_birthday": "02.02.1993", "coure": 3,
+            "physical_data": [90, 180], "python": True}  # variable dictionary (словарь)
+
+
+def print():
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
